d8b9ab728b3434c00e6e00cf2833b5eb0f7dbb2c
dongxie <dongxie@sohu-inc.com>
2022-04-08 14:22:44 +0800

    基本数据类型

--- gostudy/main.go
+++ gostudy/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+
 	//"gostudy/testpackage"
 	//cool "gostudy/testpackage"
 	. "gostudy/testpackage"
@@ -14,4 +16,12 @@ func main() {
 	b := "hello go go bbb"
 	fmt.Println(b)
 	fmt.Println(A)
+	fmt.Println("----基本数据类型")
+
+	var str1 string = "123"
+	atoi, _ := strconv.Atoi(str1)
+
+	fmt.Println(atoi)
+	fmt.Printf("%T", atoi)
+
 }
