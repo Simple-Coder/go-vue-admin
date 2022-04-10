dae52e89dec0c1167508807189f9d4fb16f7e471
xiedong <dxiesjz@163.com>
2022-04-10 11:26:18 +0800

    泛型

---
+++ gostudy/t/main.go
@@ -0,0 +1,14 @@
+//package main
+//
+//import "fmt"
+//
+////泛型
+//
+//func test[T any](i T) T {
+//	return i
+//}
+//
+////go tool arguments:配置-gcflags=-G=3
+//func main() {
+//	fmt.Println(test[string]("123") + "456")
+//}
