e957ae90f1dd1dedadc424a7444fcac2a480cb74
xiedong <dxiesjz@163.com>
2022-04-10 14:18:27 +0800

    文件写回前端

--- gin_study/up_down_file/main.go
+++ gin_study/up_down_file/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
+	//engine.POST("/testUpload", func(c *gin.Context) {
+	//	file, _ := c.FormFile("file")
+	//	c.SaveUploadedFile(file, "./"+file.Filename)
+	//	c.JSON(200, gin.H{
+	//		"msg": file,
+	//	})
+	//})
+	//
+
+	//文件写回
 	engine.POST("/testUpload", func(c *gin.Context) {
 		file, _ := c.FormFile("file")
 		c.SaveUploadedFile(file, "./"+file.Filename)
-		c.JSON(200, gin.H{
-			"msg": file,
-		})
+		c.Writer.
+			Header().Add("Content-Disposition", fmt.Sprintf("attachment;filename=%s", file.Filename))
+		c.File("./" + file.Filename)
 	})
 
 	engine.Run(":8080")
