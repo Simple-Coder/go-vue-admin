56382a4bdc38a15c6ec079b3261008ce1716642e
xiedong <dxiesjz@163.com>
2022-04-10 16:49:26 +0800

    casbin ...

---
+++ gin_study/casbin/main.go
@@ -0,0 +1,17 @@
+package main
+
+//import (
+//	"github.com/casbin/casbin/v2"
+//	gormadapter "github.com/casbin/gorm-adapter/v3"
+//)
+
+//import (
+//	"github.com/casbin/casbin/v2"
+//	gormadapter "github.com/casbin/gorm-adapter/v3"
+//	_ "github.com/go-sql-driver/mysql"
+//)
+
+func main() {
+	//a, _ := gormadapter.NewAdapter("mysql", "root:123456@tcp(www.xdblog.site:3306)/") // Your driver and data source.
+	//e, _ := casbin.NewEnforcer("./casbin/rbac_model.conf", a)
+}
