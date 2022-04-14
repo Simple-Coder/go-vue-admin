2ecf61d4ee596c7e428744f90a2bec9cb4ddd3a6
xiedong <dxiesjz@163.com>
2022-04-14 22:24:39 +0800

    vue cli router初步使用

---
+++ vue_study/13_vue_cli/hello-world/src/views/xd.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "谢冬",
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {},
+};
+</script>
+<style lang='css' scoped>
+</style>
---
+++ vue_study/13_vue_cli/hello-world/vue.config.js
@@ -0,0 +1,4 @@
+// 这里的webpack配置会和公共的webpack.config.js进行合并
+module.exports = {
+
+}
